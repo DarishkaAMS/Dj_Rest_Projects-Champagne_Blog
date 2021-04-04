567183cdc80132c7590191623783a81b89c454f6
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-04 17:47:51 +0300

    Restructure the template

--- champagne/views.py
+++ champagne/views.py
@@ -9,34 +9,34 @@ from .models import ChampagneBlogPost
 
 def blog_page_list_view(request):
     queryset = ChampagneBlogPost.objects.all()
-    template_name = "blog_page_list.html"
+    template_name = "champagne/blog_page_list.html"
     context = {"object_list": queryset}
     return render(request, template_name, context)
 
 
 def blog_page_create_view(request):
-    template_name = "blog_page_create.html"
+    template_name = "champagne/blog_page_create.html"
     context = {"from": None}
     return render(request, template_name, context)
 
 
 def blog_page_detail_view(request, slug):
     obj = get_object_or_404(ChampagneBlogPost, slug=slug)
-    template_name = "blog_page_detail.html"
+    template_name = "champagne/blog_page_detail.html"
     context = {"object": obj}
     return render(request, template_name, context)
 
 
 def blog_page_update_view(request, slug):
     obj = get_object_or_404(ChampagneBlogPost, slug=slug)
-    template_name = "blog_page_update.html"
+    template_name = "champagne/blog_page_update.html"
     context = {"object": obj, "form": None}
     return render(request, template_name, context)
 
 
 def blog_page_delete_view(request, slug):
     obj = get_object_or_404(ChampagneBlogPost, slug=slug)
-    template_name = "blog_page_delete.html"
+    template_name = "champagne/blog_page_delete.html"
     context = {"object": obj}
     return render(request, template_name, context)
 
