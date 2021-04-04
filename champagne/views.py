5e270f7990b6a1a7f8f579d7c52628e24e673da7
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-04 07:03:24 +0300

    Add Bootstrap styling

--- champagne/views.py
+++ champagne/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home_page_view(request):
-    return HttpResponse("Good morning beautiful! Welcome Back Home")
+    return render(request, "hello.html")
 
 
 def about_page_view(request):
