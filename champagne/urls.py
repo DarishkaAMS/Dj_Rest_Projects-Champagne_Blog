c873184841235c47504498509aba33a4535784c3
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-04 15:11:22 +0300

    Add blog/<int:post_id>

--- champagne/urls.py
+++ champagne/urls.py
@@ -6,6 +6,7 @@ from champagne.views import about_page_view, blog_page_detail_view, contact_page
 urlpatterns = [
     path('', home_page_view),
     path('blog/<int:post_id>/', blog_page_detail_view),
+    # re_path(r'blog/(?P<post_id>\d+)/$', blog_page_detail_view),
     path('contact/', contact_page_view),
     path('example/', example_page_view),
     # path('blog/', blog_page_detail_view),
