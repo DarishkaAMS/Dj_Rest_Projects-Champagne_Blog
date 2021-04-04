721c058f76498b647946180e2074682a9bfcc7e8
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-04 16:10:08 +0300

    Add CRUD @ views.py

--- champagne/views.py
+++ champagne/views.py
@@ -43,20 +43,34 @@ def blog_page_detail_view(request, slug):
 
 
 def blog_page_list_view(request):
-    pass
+    template_name = "blog_page_list.html"
+    context = {"object_list": []}
+    return render(request, template_name, context)
 
 
 def blog_page_create_view(request):
-    pass
+    template_name = "blog_page_create.html"
+    context = {"from": None}
+    return render(request, template_name, context)
 
 
-def blog_page_retrieve_view(request):
-    pass
+def blog_page_retrieve_view(request, slug):
+    obj = get_object_or_404(ChampagneBlogPost, slug=slug)
+    template_name = "blog_page_retrieve.html"
+    context = {"object": obj}
+    return render(request, template_name, context)
 
 
-def blog_page_update_view(request):
-    pass
+def blog_page_update_view(request, slug):
+    obj = get_object_or_404(ChampagneBlogPost, slug=slug)
+    template_name = "blog_page_update.html"
+    context = {"object": obj, "form": None}
+    return render(request, template_name, context)
 
 
-def blog_page_delete_view(request):
-    pass
+def blog_page_delete_view(request, slug):
+    obj = get_object_or_404(ChampagneBlogPost, slug=slug)
+    template_name = "blog_page_delete.html"
+    context = {"object": obj}
+    return render(request, template_name, context)
+
