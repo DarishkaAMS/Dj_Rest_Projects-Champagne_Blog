5ba7376235bc785433c15da96243c2c6f6bbf673
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-04 19:49:50 +0300

    Add user @ ChampagneBlogPost

--- champagne/models.py
+++ champagne/models.py
@@ -6,7 +6,7 @@ from django.db import models
 User = settings.AUTH_USER_MODEL
 
 
-class ChampagneBlogPost(models.Model):
+class ChampagneBlogPost(models.Model):  # champagneblogpost_set -> qs
     user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
