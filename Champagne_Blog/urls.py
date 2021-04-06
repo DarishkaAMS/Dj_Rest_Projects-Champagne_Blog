94fa4723e01cfdbb8ada7eb8fe4388b100ba0224
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-06 06:17:58 +0300

    Set settings.STATIC_URL and MEDIA_URL @ urls.py

--- Champagne_Blog/urls.py
+++ Champagne_Blog/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
 
@@ -31,3 +32,8 @@ urlpatterns = [
     re_path(r'^about/$', about_page_view),
     re_path(r'^pages?/$', about_page_view),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
